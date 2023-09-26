b4857dd34be106b9ad6c24561b5de60d46752cd9
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-09-27 00:24:05 +0200

    Outlet + main page with working link to userprofile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
 //yarn dev
 
 function App() {
@@ -11,8 +12,12 @@ function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Layout />}> 
       <Route index element={<IndexPage/>}/>
       <Route path="/login" element={<LoginPage/>}/>
+
+      </Route>
+
     </Routes>
 
     
