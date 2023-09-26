7a3db8549870d7388796b954c3c7eef42cc3bca7
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-09-26 17:53:16 +0200

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-red-500">
+       1
+      test</div>
+    
+  )
+}
+
+export default App
