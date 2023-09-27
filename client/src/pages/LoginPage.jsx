1991cd9c4772c69a9a9ccd1cfa1eb8597146fa3e
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-09-27 19:24:54 +0200

    logiPage form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,15 @@
 export default function LoginPage(){
-    return (<>dsadsadasdsada</>)
+    return (
+
+        <div>
+            <h1 className="text-center">Login</h1>
+<form className="max-w-md mx-auto">
+    <input type="email" placeholder="your@mail.com"/>
+    <input type="text" placeholder="password"/>
+    <button className="primary">Login</button>
+</form>
+
+        </div>
+
+    )
 }
