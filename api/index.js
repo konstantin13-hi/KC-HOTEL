1592b16c0613262af43723b665f4cdccdc286654
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-09-28 18:47:09 +0200

    added User Schema(bcrypt)

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
+const User = require('./modules/User')
+const bcrypt =require('bcryptjs')
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 require('dotenv').config()
 
@@ -22,8 +25,13 @@ app.get('/test',(req,res)=>{
 
 app.listen(4000);
 
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
     const {name,email,password}=req.body;
-    res.json({name,email,password});
+   const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password,bcryptSalt),
+    })
+    res.json(userDoc);
 })
 //sBbhgiwCCQ6ImopI
