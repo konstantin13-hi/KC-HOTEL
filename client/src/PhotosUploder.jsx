b466542b7caff8d1f3cb95675a0142f03690c279
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-10-04 21:58:12 +0200

    refactoring-Added component for upload form photos (divice and links)

---
+++ client/src/PhotosUploder.jsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function PhotosUploder(){
+    const[addedPhotos,setAddedPhotos] = useState([]);
+    const[photoLink,setPhotoLink] = useState('');
+
+    async function addPpotoBylink(event){
+        event.preventDefault();
+         const {data : filename} =   await axios.post('/upload-by-link',{link:photoLink})
+         setAddedPhotos(prev =>{
+            return[...prev,filename]
+         })
+         setPhotoLink('')
+        }
+    
+        function uploadPhoto(ev) {
+            const files = ev.target.files;
+          
+            const data = new FormData();
+            for (let i = 0; i < files.length; i++) {
+              data.append('photos', files[i]);
+            }
+            console.log(data);
+          
+            axios.post('/upload', data, {
+              headers: { 'Content-Type': 'multipart/form-data' }
+            })
+            .then(response => {
+              const { data: filenames } = response;
+              setAddedPhotos(prev => {
+                return [...prev, ...filenames];
+              });
+            })
+            .catch(error => {
+              console.error('Error when enabling photos:', error);
+            });
+          }
+
+
+   return(
+    <>
+     <div className="flex gap-2">
+                            <input type="text" value={photoLink} onChange={event => {setPhotoLink(event.target.value)}} placeholder="Add using a link"/>
+                            <button onClick ={addPpotoBylink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+                        </div>
+                        
+                         <div className="grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-col-6">
+                            {addedPhotos.length> 0 && addedPhotos.map(link=>(
+                                <div className="h-32 flex" key={link}>
+                                   <img className="rounded-2xl w-full object-coverl" src={'http://localhost:4000/uploads/'+link} alt="" /> 
+                                </div>
+                            ))}
+                            <label onChange={uploadPhoto}  className="h-32 curser-pointer flex gap-1 items-center justify-center border bg-transparent rounded-2xl p-8">
+                            <input type="file" multiple className="hidden"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                                </svg>
+                                Upload 
+
+
+                            </label>
+                         </div>
+
+
+    </>
+    )
+}
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import Perks from "../Perks";
 import axios from "axios";
+import PhotosUploder from "../PhotosUploder";
+
 
 export default function PlacesPage(){
     const {action} = useParams();
@@ -38,43 +40,13 @@ export default function PlacesPage(){
         )
     }
 
-   async function addPpotoBylink(event){
-    event.preventDefault();
-     const {data : filename} =   await axios.post('/upload-by-link',{link:photoLink})
-     setAddedPhotos(prev =>{
-        return[...prev,filename]
-     })
-     setPhotoLink('')
-    }
-
-    function uploadPhoto(ev) {
-        const files = ev.target.files;
-      
-        const data = new FormData();
-        for (let i = 0; i < files.length; i++) {
-          data.append('photos', files[i]);
-        }
-        console.log(data);
-      
-        axios.post('/upload', data, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        .then(response => {
-          const { data: filenames } = response;
-          setAddedPhotos(prev => {
-            return [...prev, ...filenames];
-          });
-        })
-        .catch(error => {
-          console.error('Error when enabling photos:', error);
-        });
-      }
+  
     
     
     
 
     return (
-        <div>
+        <div className="p-8">
             {action !=='new' &&(
 
                     <div className="text-center">
@@ -99,27 +71,7 @@ export default function PlacesPage(){
                         <input type ="text" value={address} onChange={event => {setAddress(event.target.value)}} placeholder="address............"/>
                         <h2 className="text-2xl mt-4"> Photos </h2>
                         <p className="text-gray-500">more = better </p>
-                        <div className="flex gap-2">
-                            <input type="text" value={photoLink} onChange={event => {setPhotoLink(event.target.value)}} placeholder="Add using a link"/>
-                            <button onClick ={addPpotoBylink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-                        </div>
-                        
-                         <div className="grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-col-6">
-                            {addedPhotos.length> 0 && addedPhotos.map(link=>(
-                                <div className="h-32 flex">
-                                   <img className="rounded-2xl w-full object-coverl" src={'http://localhost:4000/uploads/'+link} alt="" /> 
-                                </div>
-                            ))}
-                            <label onChange={uploadPhoto}  className="h-32 curser-pointer flex gap-1 items-center justify-center border bg-transparent rounded-2xl p-8">
-                            <input type="file" multiple className="hidden"/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                                </svg>
-                                Upload 
-
-
-                            </label>
-                         </div>
+                       <PhotosUploder/>
                          <h2 className="text-2xl mt-4"> Description </h2>
                         <p className="text-gray-500">description of the place</p>
                         <textarea value={description} onChange={event => {setDescription(event.target.value)}}/>
@@ -139,22 +91,22 @@ export default function PlacesPage(){
             <p className="text-gray-500">add check in and out</p>
 
             <div className="grid gap-2 sm:grid-cols-3">
-              <div >
-                <h3 className="mt-2 -mb-1">Check in time </h3>
-                <input type="text" value={checkIn} onChange={event => {setCheckIn(event.target.value)}} placeholder="14:00"/> 
-             </div>
-
-             <div>
-             <h3 className="mt-2 -mb-1">Check out time </h3>
-                <input type="text" value={checkOut} onChange={event => {setCheckOut(event.target.value)}} /> 
-             </div>
-
-             <div>
-             <h3 className="mt-2 -mb-1">Max number of guest </h3>
-                <input type="number" value={maxGuest} onChange={event => {setMaxGuest(event.target.value)}} /> 
-             </div>
-                          
+                    <div >
+                        <h3 className="mt-2 -mb-1">Check in time </h3>
+                        <input type="text" value={checkIn} onChange={event => {setCheckIn(event.target.value)}} placeholder="14:00"/> 
                     </div>
+
+                    <div>
+                        <h3 className="mt-2 -mb-1">Check out time </h3>
+                        <input type="text" value={checkOut} onChange={event => {setCheckOut(event.target.value)}} /> 
+                    </div>
+
+                    <div>
+                         <h3 className="mt-2 -mb-1">Max number of guest </h3>
+                         <input type="number" value={maxGuest} onChange={event => {setMaxGuest(event.target.value)}} /> 
+                    </div>
+                          
+            </div>
                     <button className="primary my-4">Save</button>
               
                 </div>
